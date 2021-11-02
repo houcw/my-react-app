80e6e0680ca78149e95e7d829378a051b3e792cf
houcw <h13720613310@163.com>
2021-11-02 11:26:22 +0800

    暂存

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Description:
+ * @Author: houcw
+ * @Date: 2020-12-03 17:11:06
+ * @LastEditTime: 2021-11-02 11:20:58
+ * @LastEditors: houcw
+ * @Reference:
+ */
+
+import "./App.css";
+import { Button } from "antd";
+const name = "Josh Perez";
+function nameFun(user) {
+  return user.firstName + user.lastName;
+}
+const obj = {
+  firstName: "hou",
+  lastName: "chengwei",
+};
+const element = (
+  <div className="center">
+    <p>{name}</p>
+    <p>hello {nameFun(obj)}</p>
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  </div>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return element;
 }
 
 export default App;
